806e6ce3897e8d92b2347e7e86f0875e190dcd3b
HemanthKumarRaju <somalrajuhemanth@gmail.com>
2026-08-24 17:13:07 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/GitPractiseBackend/GitPractiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GitPractiseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GitPractiseBackend/GitPractiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GitPractiseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
